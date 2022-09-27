a3d455ea71b617790649467812c674b2928c2548
LelyH <lely2011@gmail.com>
2022-09-26 21:27:08 -0700

    first draft of JS and clearn up form on html

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var ButtonSelector = document.querySelector("#submit-btn");
+// var ResultsOutput = 
+
+// need to define ResultsOutput
+function SearchResults() {
+    return ResultsOutput;
+}
+
+ButtonSelector.addEventListener('click', SearchResults);
