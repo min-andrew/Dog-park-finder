35c458db46fe953c9eaf75f702a940dd195f2942
Andrew Min <minandrew0711@gmail.com>
2022-09-26 21:16:56 -0700

    finished search function and made an example search bar

--- search.js
+++ search.js
@@ -1,11 +1,18 @@
 // search function includes variables to connect to the Form, event listener, and returning data 
-var input
+var searchForm = document.querySelector("#search-form");
+var searchInput = document.querySelector("#search-input");
+var submitbtn = document.querySelector("#submitbtn");
+
 function search(event) {
     event.preventDefault();
 
-    if (input === undefined) {
+    if (searchResult.value === "") {
         console.error('You need a search input value!');
         return;
     }
-}
 
+    var searchResult = searchInput.value;
+    return searchResult;
+};
+
+submitbtn.addEventListener("click", search);
