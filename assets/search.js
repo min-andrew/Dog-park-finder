01ec2d0b0089680f316e106ebdcabd042363c47f
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-09-28 17:12:22 -0700

    Added several updates in this commit. Add a CSS file, along with CSS code to start the styling of the search results page. Added placeholder collage image for the background. Added API link for google maps and some JS code to link the map, though it's getting an error message saying it can't find the function. Planning to ask for help on that in the office hours. I also have some commented out code in JS which was something else I was trying to get the map to load. Leaving it for now until we know how to fix the error.

--- assets/search.js
+++ assets/search.js
@@ -3,16 +3,55 @@ var searchForm = document.querySelector("#search-form");
 var searchInput = document.querySelector("#search-input");
 var submitbtn = document.querySelector("#submitbtn");
 
-function search(event) {
-    event.preventDefault();
+// function search(event) {
+//     event.preventDefault();
 
-    if (searchResult.value === "") {
-        console.error('You need a search input value!');
-        return;
+//     if (searchResult.value === "") {
+//         console.error('You need a search input value!');
+//         return;
+//     }
+
+//     var searchResult = searchInput.value;
+//     return searchResult;
+// };
+
+// function initMap() {
+//     let options= {
+//         zoom: 8,
+//         center: {lat: 47.6062,lng: -122.3321}
+//     }
+//     let map = new
+//     google.maps.Map(document.getElementById("map",options));
+// }
+
+var map;
+var service;
+var infowindow;
+
+function initMap() {
+  var seattle = new google.maps.LatLng(47.6062,-122.3321);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: seattle,
+      zoom: 10
+    });
+
+  var request = {
+    location: seattle,
+    radius: '500',
+    type: ['dog park']
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
     }
-
-    var searchResult = searchInput.value;
-    return searchResult;
-};
+  }
+}
 
 submitbtn.addEventListener("click", search);
