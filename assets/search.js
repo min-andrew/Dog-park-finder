f330f7e419af87f42e3cf7bcef388b831dd41cb7
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-09-29 17:19:13 -0700

    Updated the CSS to fix spacing error that I caused during revisions. Added a small sample piece of code to the search.js that is commented out - the team can discuss whether that's the right direction for how we provide the search parameters.

--- assets/search.js
+++ assets/search.js
@@ -6,7 +6,7 @@ var submitbtn = document.querySelector("#submitbtn");
 function search(event) {
     event.preventDefault();
 
-    if (searchResult.value === "") {
+    if (searchInput.value === "") {
         console.error('You need a search input value!');
         return;
     }
@@ -16,3 +16,10 @@ function search(event) {
 };
 
 submitbtn.addEventListener("click", search);
+
+
+// Code sample for using an inputed city to pull the lon/lat from this api//
+// fetch("https://api.geoapify.com/v1/geocode/autocomplete?text=Trenton&format=json&apiKey=f05b36b362604ffcbb3871392e2bf6b9")
+//   .then(response => response.json())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
